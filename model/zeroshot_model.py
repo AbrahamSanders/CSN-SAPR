493059d61844bb2d61f21eb8f012a56bab01fa95
Avi Sanders <abraham.sanders@gmail.com>
2022-04-28 15:34:51 -0400

    Added switch to use lm_probs or lm_loss scoring mode

--- model/zeroshot_model.py
+++ model/zeroshot_model.py
@@ -8,6 +8,11 @@ class CSN_Zeroshot(nn.Module):
     def __init__(self, args):
         super(CSN_Zeroshot, self).__init__()
         
+        self.score_mode = args.score_mode
+        score_modes = ["lm_probs", "lm_loss"]
+        if self.score_mode not in score_modes:
+            raise ValueError(f"score_mode must be one of {score_modes}.")
+        
         self.tokenizer = AutoTokenizer.from_pretrained(args.bert_pretrained_dir)
         if not self.tokenizer._pad_token:
             self.tokenizer.pad_token = self.tokenizer.eos_token
@@ -45,16 +50,19 @@ class CSN_Zeroshot(nn.Module):
                    for css, alias, quoted in zip(CSSs, candidate_aliases, quoted_sentences)]
         prompts = [p.replace("“", '"').replace("”", '"') for p in prompts]
         
-        yes_ppl = self.get_ppl(prompts, self.response_yes, device)
-        no_ppl = self.get_ppl(prompts, self.response_no, device)
+        yes_score = self.get_score(prompts, self.response_yes, device)
+        no_score = self.get_score(prompts, self.response_no, device)
         
-        scores = (no_ppl - yes_ppl) / (no_ppl + yes_ppl)
+        scores = (yes_score - no_score) / (yes_score + no_score)
+        if self.score_mode == "lm_loss":
+            scores = -scores
         scores_false = [scores[i] for i in range(scores.size(0)) if i != true_index]
         scores_true = [scores[true_index] for i in range(scores.size(0) - 1)]
         
         return scores, scores_false, scores_true
     
-    def get_ppl(self, prompts, response, device):
+    def get_score(self, prompts, response, device):
+        # Get the logits and targets
         base_targets = None
         if self.model.config.is_encoder_decoder:
             inputs = self.tokenizer(prompts, padding=True, return_tensors="pt").to(device)
@@ -72,25 +80,42 @@ class CSN_Zeroshot(nn.Module):
             targets = inputs
             logits = self.model(**inputs).logits
         
-        ppl = torch.exp(self._get_lm_loss(logits, targets, base_targets))
-        return ppl
+        # Get the score (probs or loss)
+        score = self._get_score_from_logits(logits, targets, base_targets)
+        return score
     
-    def _get_lm_loss(self, logits, targets, base_targets):
+    def _get_score_from_logits(self, logits, targets, base_targets):
         labels = targets.input_ids
         base_labels = None if base_targets is None else base_targets.input_ids
         
-        # Get the token-wise loss
+        # perform model-specific logit and label alignment
         logits, labels = preprocess_logits_and_labels_for_crossentropy(self.model, logits, labels)
-        logits = torch.permute(logits, (0, 2, 1))
-        loss = self.loss_fct(logits, labels)
         
         # Mask out any position in labels that is identical to base_labels
-        # and also mask out any padding
-        mask = torch.ones_like(loss)
+        # and also mask out any special tokens (e.g., padding, eos, etc.)
+        mask = torch.ones_like(labels, dtype=torch.bool)
         if base_labels is not None:
             _, base_labels = preprocess_logits_and_labels_for_crossentropy(self.model, None, base_labels)
             mask[:, :base_labels.shape[1]] = labels[:, :base_labels.shape[1]] != base_labels
-        mask[labels == self.tokenizer.pad_token_id] = 0.
-        loss = (loss*mask).sum(dim=-1) / mask.sum(dim=-1)
-        return loss
+        for special_token_id in self.tokenizer.all_special_ids:
+            mask[labels == special_token_id] = False
+        
+        if self.score_mode == "lm_loss":
+            # Get the token-wise loss
+            logits = torch.permute(logits, (0, 2, 1))
+            loss = self.loss_fct(logits, labels)
+            loss = (loss*mask).sum(dim=-1) / mask.sum(dim=-1)
+            # Convert to perplexity
+            score = torch.exp(loss)
+        else:
+            # Convert logits to logprobs
+            selected_logits = logits[mask]
+            selected_logprobs = nn.functional.log_softmax(selected_logits, dim=-1)
+            selected_labels = labels[mask]
+            selected_label_logprobs = selected_logprobs[torch.arange(selected_labels.shape[0]), selected_labels]
+            selected_label_logprobs = selected_label_logprobs.view(labels.shape[0], -1)
+            # Average the logprobs and convert to probabilities
+            score = torch.exp(selected_label_logprobs.mean(dim=-1))
+            
+        return score
         
